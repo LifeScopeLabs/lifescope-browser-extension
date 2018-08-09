0ae73fad22d6e232022aaa83483d9335e9cca0f0
William Broza <lbroza@bitscoop.com>
2018-08-08 20:45:54 -0700

    Verbage tweaks.

--- src/options/router/pages/Index.vue
+++ src/options/router/pages/Index.vue
@@ -1,58 +1,39 @@
 <template>
 	<div class="page">
 		<div class="boxed-group">
-			<div class="title">LifeScope Browser Extension Settings</div>
+			<div class="title">LifeScope Browser Extension Options</div>
 
 			<div class="padded paragraphed mobile-margin">
 				<p>
 					LifeScope can help you organize and search your time on the web.
-					We only record visits to sites you care about and have explicitly approved us to track.
-					You must manually approve sites to be tracked.
 					This can be done by navigating to a site, clicking the LifeScope extension icon in the upper right, and selecting either 'Start tracking this domain' or 'Start tracking this URL'.
 					You can also manually add the domain or URL to the list below.
 				</p>
-
 				<p v-if="browserName === 'Chrome' || browserName === 'Firefox'">
-					When you add a domain or URL to the whitelist, we will retrieve your entire history on that domain or URL from your browser.
-					Any further visits to that domain or URL will also be recorded.
-					Note that these actions will only occur if you are logged into LifeScope on that device.
+					When you add a domain or URL to the whitelist, all visits to that domain or URL will be indexed from your browser.
 				</p>
 				<p v-else-if="browserName === 'Microsoft Edge'">
 					Microsoft Edge unfortunately does not let us access your browser history at this time.
 					We therefore cannot retrieve any previous visits to domains or URL that you have approved, and can only track future visits.
 					Note that you must be logged into LifeScope on that device for future visits to be recorded.
 				</p>
-
-				<p>Deleting a domain or URL from the list will stop the extension from tracking that domain or URL going forward, but it will <u>not</u> delete anything already recorded for that domain or URL.</p>
-
+				<p>Deleting from the list will stop tracking but it will <u>not</u> delete vists already recorded for that domain or URL.</p>
 				<p>
-					At this time, there is no option for selectively deleting data gathered using this extension.
-					You would need to delete the Connection in the main LifeScope app, which will clear your whitelist, then add the domains or URLs you do want to keep back to the list.
+					At this time, there is no option for selectively deleting data gathered using this extension but you can delete indexted web visits in the LifeScope app.
 				</p>
-
 				<div v-if="$data.connection && $data.connection.enabled === false">
 					<h2>Extension Connection is disabled</h2>
 
 					<div>
 						<p>The Connection for this browser extension is currently disabled. No new visits to any of the domains or URLs in your list are being recorded.</p>
-						<p>Any new domains or URLs that you add to the list will not have their history mapped until the Connection is enabled.</p>
-						<p>Any activity on listed sites that occurs when the Connection is disabled will not be restored upon the Connection being enabled.</p>
+						<p>Any new domains or URLs that you add to the list will not be indexed until the Browser Extension Connection is enabled.</p>
 					</div>
 				</div>
-
 				<div class="whitelist">
-					<h3>Tracked Domain/URL List</h3>
-
+					<h3>Tracked DomainURL List</h3>
 					<div>
-						<p>You can enter the domains or URLs of websites you want to track here. Some examples of domains are
-							'google.com', 'wikipedia.org', and 'dmv.ca.gov'. Basically, it's everything after the
-							'https://' that ends with '.com', '.org', etc. An example of a URL is 'https://en.wikipedia.org/Main_Page';
-							it's the http(s)://, plus the domain, plus everything after each '/', but not any '?' or '#' or anything following those characters.</p>
-						<p>If you don't want to enter this information manually, go to the site you want to track, click
-							on the button for the LifeScope extension, and select 'Start tracking this domain' or 'Start tracking this URL'.</p>
+						<p>Enter web domains or URLs of websites for LifeScope to track here (Such as 'wikipedia.org' or 'hulu.com/rick-and-morty'.</p>
 					</div>
-
-					<h3>Enter new domains/URLs here</h3>
 					<form v-on:submit.prevent="addWhitelistEntry">
 						<div class="add-domain flexbox flex-x-center">
 							<div class="text-box">
@@ -62,8 +43,6 @@
 							<span v-if="$data.invalidDomain === true">That was not a valid domain or site.</span>
 						</div>
 					</form>
-
-					<h3>Your currently tracked domains/URLs</h3>
 					<div class="entries">
 						<div v-for="entry in sortedEntries" class="entry">
 							<span>{{ entry }}</span>
